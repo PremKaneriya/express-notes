7c85a51d3e999f63e124a8abb331a9f0835810f2
Prem-Codezee <kprem.codezee@gmail.com>
2025-02-17 22:09:46 +0530

    fix 2

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
+  console.log("cookie recived", req.cookies);
   const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ error: "Unauthorized" });
